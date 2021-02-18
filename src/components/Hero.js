22ee59cdf9bf344e976128eb13b911589f69235c
plhnk <paul.hanaoka@gmail.com>
2021-02-18 01:26:49 -0800

    dont show my work :joy:

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 
 import { jsx, Box, useColorMode } from 'theme-ui';
-import { useState, setState, useEffect } from 'react';
+import { useState } from 'react';
 import Heart from '../components/Heart';
 import Img from 'gatsby-image';
 
@@ -14,60 +14,11 @@ export default function Hero({ imageArray, text, ...props }) {
 
   const currentImages = mode === 'light' ? lightImages : darkImages;
 
-  // const randomImage =
-  //   currentImages[Math.floor(Math.random() * currentImages.length)];
-
   const randomNo = Math.floor(Math.random() * currentImages.length);
 
   // //https://stackoverflow.com/questions/26944987/show-next-previous-item-of-an-array
   const [initialPic, setPic] = useState(randomNo);
-  // const [initialPic, setPic] = useState(randomImage);
 
-  function nextItem() {
-    const i = currentImages.indexOf(initialPic) + 1;
-    if (i === i % currentImages.length) {
-      return 0;
-    }
-    return i; // output is an object
-  }
-  // function nextItem(currentItem, inputArray) {
-  //   const i = inputArray.indexOf(currentItem) + 1;
-  //   if (i === i % inputArray.length) {
-  //     return inputArray[0];
-  //   }
-  //   return inputArray[i]; // output is an object
-  // }
-  const newPic = nextItem(initialPic, currentImages);
-
-  // // if i'm updating an image — setPic should get me a new randomPic — but need to check if that randomPic === currentPic, if so, get a new randomPic
-
-  // const isNextItem = nextItem(randomImage, currentImages);
-  // console.log(isNextItem, 'is next?');
-
-  // const newPic = setPic(
-  //   initialPic === randomImage ? preventRedundancy(initialPic) : randomImage
-  // );
-
-  // const newPic = setPic(randomImage);
-
-  // //https://stackoverflow.com/questions/26944987/show-next-previous-item-of-an-array
-  //   const next = nextItem(randomImage, currentImages);
-
-  // const x = setPic(nextItem(initialPic, currentImages));
-
-  // const randomImage = imageArray[0].dark;
-
-  // console.log(preventRedundancy(randomImage), 'refresh?');
-  // console.log(preventRedundancy(initialPic), 'not redundant');
-  // console.log(nextItem(randomImage, currentImages), 'next item?');
-  // console.log(randomImage, 'current item');
-  // console.log(next, 'next pic?');
-  // console.log(setPic(next), 'set pic?');
-  // console.log(x, 'next pic x?');
-  // console.log(data,'image?')
-  // console.log(setPic(newPic),'new pic?')
-
-  // const refresh = useForceUpdate();
   const imagePlaceholder = currentImages[initialPic];
 
   const image = (
